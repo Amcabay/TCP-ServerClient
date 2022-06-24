5cd0cc49b1ea65f0952f77ece7e9f5af7f395d8a
Amcabay <84550409+Amcabay@users.noreply.github.com>
2022-06-24 15:43:16 +0700

    Update main.cpp

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,104 +1,233 @@
 #include <iostream>
-#include <string.h>
-#include <string>
-#include <thread>
+//Windows Socket -> Untuk mengakses network socket
 #include <WS2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+#pragma comment (lib, "ws2_32.lib")
 
 using namespace std;
 
-void receiveSocket(SOCKET sock)
-{
-	char buf[4096];
-	while (1)
-	{
-		ZeroMemory(buf, 4096);
-		int bytesReceived = recv(sock, buf, 4096, 0);
-
-		if (bytesReceived > 0)
-		{
-			cout << "Server> " << string(buf, 0, bytesReceived) << endl;
-		}
-
-		else
-		{
-			cout << endl;
-			cout << "Server Disconnected" << endl;
-			break;
-		}
-	}
-}
-
 void main()
 {
-	string ipAddress = "127.0.0.1";				//Ipadress of the server
-	int port = 54000;							//Listening port on the server
-
 	//INISIALISASI WINSOCK
-	WSAData data;
-	WORD ver = MAKEWORD(2, 2);
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);		//Version 2.2
 
-	int wsResult = WSAStartup(ver, &data);
-	if (wsResult != 0)
+	int wsock = WSAStartup(ver, &wsData);		
+
+	if (wsock != 0)
 	{
-		cerr << "winsock can't start ! " << wsResult << endl;
+		cerr << "initialize error ! " << endl;
 		return;
 	}
-
-	//CREATE SOCK
-		/*Socket adalah program untuk berkomunikasi dengan network layer untuk mengirimkan data ke remote manchine
-		,Sama seperti di server*/
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-	
-	if (sock == INVALID_SOCKET)
+	else
 	{
-		cerr << "can't create socket" << WSAGetLastError() << endl;
-		WSACleanup();
-		return;
+		cout << "Winsock Ready" << endl;
 	}
 
-	//FILL IN A HINT STRUCTURE
+	//CREATE A SOCK
+	//Socket berfungsi untuk mengakses suatu port di ip address
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);			//AF_Inet = version 4
+
+	//tell server untuk listen ke port tertentu
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "Socket Error ! err# " << endl;
+		return;
+	}
+	else
+	{
+		cout << " Server Listening" << endl;
+	}
+
+
+	//BIND IP ADDRESS AND PORT TO A SOCKET
 	sockaddr_in hint;
 	hint.sin_family = AF_INET;
-	hint.sin_port = htons(port);
+	hint.sin_port = htons(54000);					//htons = host to network short
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;			//Bisa menggunakan inet_pton
 
-	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
 
-	// CONNECT TO SERVER
-	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
-	
-	if (connResult == SOCKET_ERROR)
+	//TELL WINSOCK THE SOCKET FOR LISTENING
+	listen(listening, SOMAXCONN != 0);
+
+	fd_set master;				//Why master? karena itu master set
+	FD_ZERO(&master);
+
+	//MENAMBAHKAN LISTENING SOCKET KE SET
+	FD_SET(listening, &master);		
+
+	bool running = true;
+
+	//RUNNING SERVER UNTUK HANDLE BEBERAPA KONEKSI
+	while (running)
 	{
-		cerr << "Can't connect to server " << WSAGetLastError() << endl;
-		closesocket(sock);
-		WSACleanup();
-		return;
+		//Menggunakan copy, karena jika setiap memanggil function select maka akan dihancurkan file descriptor set
+		fd_set copy = master;
+
+		//MELIHAT SIAPA YANG KE SERVER
+		int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
+
+		// LOOPING SEMUA KONEKSI SAAT INI
+		for (int i = 0; i < socketCount; i++)
+		{
+			SOCKET sock = copy.fd_array[i];		//i = berapa client yang harus di Accept the connection
+
+			//APAKAH KOMUNIKASI MASUK?
+			if (sock == listening)
+			{
+				// MENERIMA KONEKSI BARU
+				SOCKET client = accept(listening, nullptr, nullptr);
+
+				//TAMBAHKAN KONEKSI BARU KE DAFTAR CLIENT YANG TERHUBUNG
+				FD_SET(client, &master);
+
+				// KIRIM WELCOME MESSAGE KE CLIENT
+				string welcomeMsg = "Welcome to Chat\r\n";
+
+				send(client, welcomeMsg.c_str(), welcomeMsg.size() + 1, 0);
+
+				// BROADCAST WELCOME MESSAGE
+				ostringstream ss;
+				ss << "    Welcome Client" << " #" << client << " connected.\r\n";
+				string strOut = ss.str();
+
+				//MENERIMA MESSAGE
+				for (int i = 0; i < master.fd_count; i++)
+				{
+					SOCKET outSock = master.fd_array[i];
+					if (outSock != listening && outSock != client)
+					{
+						send(outSock, strOut.c_str(), strOut.size() + 1, 0);
+					}
+				}
+				// logging welcome message to server
+				cout << strOut << endl;
+			}
+
+			//MESSAGE MASUK
+			else
+			{
+				char buf[4096];
+				ZeroMemory(buf, 4096);
+
+				// Receive Message
+				int bytesIn = recv(sock, buf, 4096, 0); 
+				if (bytesIn <= 0)
+				{
+					cout << "Client #" << sock << " Disconnected" << endl;
+					// Drop the client
+					closesocket(sock);
+					FD_CLR(sock, &master);
+				}
+				else
+				{
+
+					// Send message to other client, and definiately not the listening socket
+					ostringstream ss;
+					ss << "Client #" << sock << ":" << buf << "\r\n";
+					string strOut = ss.str();
+					for (int i = 0; i < master.fd_count; i++)
+					{
+						SOCKET outSock = master.fd_array[i];
+						if (outSock != listening && sock != outSock)
+						{
+							send(outSock, strOut.c_str(), strOut.size() + 1, 0);
+						}
+					}
+					// logging user`s message to server
+					cout << strOut << endl;
+
+					
+				}
+			}
+		}
 	}
 
-	//THREAD TO RECEIVE DATA
-	thread receiveThread (receiveSocket, sock);
+	//CLOSE LISTENING SOCKET
+	FD_CLR(listening, &master);
+	closesocket(listening);
 
-	string userInput;
-
-	do
-	{
-		//PROMPT THE USER FOR TEXT
-		getline(cin, userInput);
-		
-		if (userInput.size() > 0)	//Mengecek apakah user mengetik sesuatu
-		{
-			
-			//SEND THE TEXT
-			send(sock, userInput.c_str(), userInput.size() + 1, 0);
-			cout << endl;
-		}
-	} while (userInput.size() > 0);
-
-	//CLOSE EVERYTHING
-	closesocket(sock);
+	//WAIT FOR CONNECTION
 	WSACleanup();
 
-	system("pause");
+	//system("pause");
 	return;
 
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*char host[NI_MAXHOST];										// remote name client
+	char service[NI_MAXSERV];									// port yang client connected
+
+	ZeroMemory(host, NI_MAXHOST);								//if using linux/IOS = memset(host, 0, NI_MAXHOST)
+	ZeroMemory(service, NI_MAXSERV);
+
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	{
+		cout << host << " connected to port " << service << endl;
+	}
+	else
+	{
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+	}
+
+	// CLOSE LISTENING SOCKET
+	closesocket(listening);
+
+	//WHILE LOOP: ACCEPT AND ECHO MESSAGE BACK TO CLIENT
+	char buf[4096];
+
+	while (true)
+	{
+		ZeroMemory(buf, 4096);
+
+		//WAIT FOR CLIENT TO SEND DATA
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+
+		if (bytesReceived == SOCKET_ERROR)
+		{
+			cout << "Error in recv(). Quitting " << endl;
+			break;
+		}
+
+		if (bytesReceived == 0)
+		{
+			cout << "Client disconnected" << endl;
+			break;
+		}
+
+		//ECHO MESSAGE BACK TO CLIENT
+		send(clientSocket, buf, bytesReceived + 1, 0);
+	}
+
+	//CLOSE THE SOCKET
+	closesocket(clientSocket);
+
+	//SHUTDOWN WINSOCK
+	WSACleanup;
+}*/
